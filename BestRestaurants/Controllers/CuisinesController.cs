10bdf6182e6f56066a5634805f2f9e06d282bfa9
Justin Lee <lee.justin001126@gmail.com>
2023-03-08 10:59:55 -0800

    Fixed routting issue, views work for cuisine views folder.
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>
    Co-authored-by: John <john.lenz27@gmail.com>.

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -5,18 +5,18 @@ using System.Linq;
 
 namespace BestRestaurants.Controllers
 {
-  public class RestaurantsController : Controller
+  public class CuisinesController : Controller
   {
     private readonly BestRestaurantsContext _db;
 
-    public RestaurantsController(BestRestaurantsContext db)
+    public CuisinesController(BestRestaurantsContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Restaurant> model = _db.Restaurants.ToList();
+      List<Cuisine> model = _db.Cuisines.ToList();
       return View(model);
     }
 
@@ -27,3 +27,34 @@ namespace BestRestaurants.Controllers
 
   }
 }
+
+
+// using Microsoft.AspNetCore.Mvc;
+// using BestRestaurants.Models;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// namespace BestRestaurants.Controllers
+// {
+//   public class RestaurantsController : Controller
+//   {
+//     private readonly BestRestaurantsContext _db;
+
+//     public RestaurantsController(BestRestaurantsContext db)
+//     {
+//       _db = db;
+//     }
+
+//     public ActionResult Index()
+//     {
+//       List<Restaurant> model = _db.Restaurants.ToList();
+//       return View(model);
+//     }
+
+//     public ActionResult Create()
+//     {
+//       return View();
+//     }
+
+//   }
+// }
--- BestRestaurants/Models/BestRestaurantsContext.cs
+++ BestRestaurants/Models/BestRestaurantsContext.cs
@@ -4,7 +4,7 @@ namespace BestRestaurants.Models
 {
   public class BestRestaurantsContext : DbContext
   {
-      public DbSet<Cuisine> CuisineType { get;set;}
+      public DbSet<Cuisine> Cuisines { get;set;}
       public DbSet<Restaurant> Restaurants { get;set;}
       
       public BestRestaurantsContext(DbContextOptions options) : base(options) { }
